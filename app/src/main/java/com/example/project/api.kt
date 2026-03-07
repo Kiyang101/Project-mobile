2d624ba774c5c7733624ef1bd7cfe0b1e3613e40
kiyang <109514563+Kiyang101@users.noreply.github.com>
2026-03-07 15:04:04 +0700

    Add ProductRepository, ProductViewModel, and ProductViewModelFactory to api.kt

--- app/src/main/java/com/example/project/api.kt
+++ app/src/main/java/com/example/project/api.kt
@@ -1,5 +1,11 @@
 package com.example.project
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -14,17 +20,6 @@ data class Product(
     val imageIds: List<Int>,
     val rating: Double,
 )
-
-//data class CartItem(
-//    val product: Product,
-//    val quantity: Int,
-//    val size: String,
-//)
-//
-//data class Cart(
-//   val items: List<CartItem>,
-//   val totalPrice: Double,
-//)
 
 interface ApiService {
     @GET("api/products")
@@ -50,4 +45,38 @@ sealed class Resource<T>(
     class Success<T>(data: T): Resource<T>(data)
     class Error<T>(message: String?, data: T? = null): Resource<T>(data, message)
     class Loading<T>: Resource<T>()
+}
+
+class ProductRepository {
+    suspend fun fetchProduct(): Resource<List<Product>> {
+        return try {
+            val response = RetrofitInstance.api.getAllProducts()
+            if (response.isSuccessful) {
+                response.body()?.let {
+                    Resource.Success(it)
+                } ?: Resource.Error("Empty body")
+            } else { Resource.Error("Error ${response.code()}") }
+        } catch (e: Exception) { Resource.Error(e.message) }
+    }
+}
+
+class ProductViewModel( private val repository: ProductRepository) : ViewModel() {
+    private val _allProducts = MutableLiveData<Resource<List<Product>>>()
+    val allProducts: LiveData<Resource<List<Product>>> = _allProducts
+
+    fun loadAllProducts(){
+        _allProducts.value = Resource.Loading()
+        viewModelScope.launch {
+            _allProducts.value = repository.fetchProduct()
+        }
+    }
+}
+
+class ProductViewModelFactory(private val repository: ProductRepository) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(ProductViewModel::class.java)) {
+            return ProductViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
 }
